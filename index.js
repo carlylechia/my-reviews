14716a0cdb594560fa935e3f0bda84e267456c1f
Chia Carlyle <chiacarlyle@gmail.com>
2021-12-06 14:03:38 +0100

    ceo pic

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const reviews = [
       id: 4,
       name: "Chia Carlyle",
       title: "CEO",
-      img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      img: "CEO.jpg",
       text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
     },
   ];
